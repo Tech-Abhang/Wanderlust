97b250755d717cb0e5591ca8d81a8a84f655967c
Abhang Pawar <abhangpawar03@gmail.com>
2025-01-12 12:15:17 +0530

    reciew form

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const ejsMate = require("ejs-mate")// helps for includes(common code)
 const wrapAsync = require("./utils/wrapAsync.js")
 const ExpressError = require("./utils/expressError.js")
 const {listingSchema} = require("./schema.js")
+const Review = require("./models/review.js")
 
 app.set("view engine","ejs")
 app.set("views",path.join(__dirname,"views"))
@@ -88,6 +89,16 @@ app.delete("/listings/:id",wrapAsync(async (req,res)=>{
     let deletedListing = await Listing.findByIdAndDelete(id)
     res.redirect("/listings")
 }))
+
+//reviews
+app.post("/listings/:id/reviews",async(req,res)=>{
+    let listing = await Listing.findById(req.params.id)
+    let newReview = new Review(req.body.review)
+
+    listing.reviews.push(newReview)
+    await newReview.save()
+    await listing.save()
+})
 
 // app.get("/testListing",async (req,res)=>{
 //     let sampleListing = new Listing({
