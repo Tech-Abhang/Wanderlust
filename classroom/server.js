ccf38c590dc52f00c44d517784721749814ec26b
Abhang Pawar <abhangpawar03@gmail.com>
2025-01-19 19:14:51 +0530

    added connect-flash

--- classroom/server.js
+++ classroom/server.js
@@ -1,2 +1,26 @@
 const express = require("express")
 const app = express()
+const session = require("express-session")
+const flash = require("connect-flash")
+
+const sessionOptions = {
+    secret :"mysupersecretstring",
+    resave:false,
+    saveUninitialized:true,
+}
+
+app.use(session(sessionOptions));
+
+app.get("/register",(req,res)=>{
+    let {name = "anonymus"} = req.query;
+    req.session.name = name;
+    res.redirect("hello")
+})
+
+app.get("/hello",(req,res)=>{
+    res.send(`hello ${req.session.name}`)
+})
+
+app.listen(3000,()=>{
+    console.log("server is listening to 3000")
+})
