13d730b4a9cd148ab4f8b3751b0981494d189dbf
Abhang Pawar <abhangpawar03@gmail.com>
2025-01-19 19:23:06 +0530

    created new folder name views in classroom

--- classroom/server.js
+++ classroom/server.js
@@ -3,6 +3,9 @@ const app = express()
 const session = require("express-session")
 const flash = require("connect-flash")
 
+app.set("view engine","ejs")
+app.set("views",path.join(__dirname,"views"))
+
 const sessionOptions = {
     secret :"mysupersecretstring",
     resave:false,
@@ -10,15 +13,16 @@ const sessionOptions = {
 }
 
 app.use(session(sessionOptions));
+app.use(flash())
 
 app.get("/register",(req,res)=>{
     let {name = "anonymus"} = req.query;
     req.session.name = name;
-    res.redirect("hello")
+    req.flash("sucess","user registered sucessfully")
 })
 
 app.get("/hello",(req,res)=>{
-    res.send(`hello ${req.session.name}`)
+    res.render("page.ejs",{name : express.session.name})
 })
 
 app.listen(3000,()=>{
