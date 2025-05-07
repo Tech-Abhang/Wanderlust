d833d37b0095a1b302d6e04be21a040bb85b3d3d
Abhang Pawar <abhangpawar03@gmail.com>
2025-05-07 16:23:32 +0530

    signin

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const User = require("../models/user")
 const router = express.Router()
+const passport = require("passport")
 
 router.get("/signup", (req, res) => {
     res.render("users/signup.ejs")
@@ -22,4 +23,20 @@ router.post("/signup",async (req,res)=>{
         res.redirect("/listings")
     }
 })
+
+router.get("/login", (req, res) => {
+    res.render("users/login.ejs")
+})
+
+router.post("/login", 
+    passport.authenticate('local', { 
+        failureRedirect: '/login',
+        failureFlash: true
+    }),
+    async (req, res) => {
+        req.flash("success", "Welcome back to Wanderlust!")
+        res.redirect("/listings")
+    }
+)
+
 module.exports = router
